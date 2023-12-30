654897c37888f5bab931b392d28fbddede2f8f66
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2023-12-30 15:36:11 +0800

    pip installable package
    1. move dlpacker as a pip installable package.
    2. add gdown to download and extract pretrained weight for the very first time use.
    3. minor optimize for mac m1/m2: use legacy Adam, seggested by tensorflow official warning message.

--- scripts/build_sidechain_DLPacker.py
+++ scripts/build_sidechain_DLPacker.py
@@ -1,13 +1,11 @@
 #! /usr/bin/env python
 
-import sys
 from pathlib import Path
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-from dlpacker import DLPacker
+from DLPacker.dlpacker import DLPacker
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-parser.add_argument('pdb', type=str, help="Input PDB file")
+parser.add_argument('-i','--pdb', type=str, help="Input PDB file")
 parser.add_argument('--out', '-o', type=str, default=None, help="Output PDB file")
 args = parser.parse_args()
 
